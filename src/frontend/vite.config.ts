15230d08c89d8a8f046ef879bee9545c2e1c9552
23127077 <dakiet23@clc.fitus.edu.vn>
2026-07-27 17:57:23 +0700

    feat: + add docker compose + add scripts to run docker compose

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   server: {
     proxy: {
       '^/api': {
-        target: 'http://127.0.0.1:8000',
+        target: process.env.VITE_BACKEND_URL || 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
     },
